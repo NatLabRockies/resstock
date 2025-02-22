2996ebf0f9a4d46a240b9fe14cf490ad41207cf1
Anthony Fontanini <anthony.fontanini@nrel.gov>
2025-02-21 18:06:16 -0700

    Add EV related warnings

--- test/test_run_analysis.rb
+++ test/test_run_analysis.rb
@@ -500,6 +500,13 @@ class TestRunAnalysis < Minitest::Test
       next if _expected_warning_message(message, 'driving hours could not be met due to insufficient vehicle charge. This issue may result from a combination EV battery parameters, charging power, and driving or discharging schedules')
       next if _expected_warning_message(message, 'do not match the hours per week calculated from the discharging schedule')
       next if _expected_warning_message(message, "Both schedule file and weekday fractions provided for 'electric_vehicle'; weekday fractions will be ignored.")
+      next if _expected_warning_message(message, "Both schedule file and weekend fractions provided for 'electric_vehicle'; weekend fractions will be ignored.")
+      next if _expected_warning_message(message, "Both schedule file and monthly multipliers provided for 'electric_vehicle'; monthly multipliers will be ignored.")
+
+      # For the EV minutes warning try replacing the number of minutes as a string rather than a number.
+      new_message = message.gsub(/\(([^)]+)\)/) { |match| $1.match?(/^\d+(\.\d+)?$/) ? "(<number of minutes>)" : match }
+      new_message =  new_message.gsub(/Only \d+ minutes was used/, 'Only <minutes value> minutes was used')
+      next if _expected_warning_message(new_message, "Insufficient away minutes (<number of minutes>) for required driving minutes (<number of minutes>)Only <minutes value> minutes was used.")
 
       flunk "Unexpected cli_output.log message found: #{message}"
     end
