0c49d7394331aaf9da96b2d4d48a35a52cc59f43
Joe Robertson <joseph.robertson@nrel.gov>
2025-05-19 08:44:40 -0700

    Try converting html to jpg.

---
+++ test/html_to_jpg.py
@@ -0,0 +1,3 @@
+import imgkit
+
+imgkit.from_file('test/base_results/comparisons/baseline/annual/results_output_energy_use.html', 'test/base_results/comparisons/baseline/annual/results_output_energy_use.jpg')
