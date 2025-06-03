581a35e21d363fa01e9d10bce28e6b12db7db478
Joe Robertson <joseph.robertson@nrel.gov>
2025-06-03 11:58:25 -0700

    Squashed 'resources/hpxml-measures/' changes from e288f63992c9c..0bc79d5d36a4d
    0bc79d5d36a4d Bugfix.
    20d93153d7bed Merge branch 'master' of https://github.com/NREL/OpenStudio-HPXML into resstock
    4c1e33e378564 Merge pull request #2018 from NREL/hpxml_42rc1
    5674adef75a54 Consolidate duplicate logic.
    859814d1a585d Update to rc2
    aa3c339e5b3a9 Oops, missed these
    639df8331bed7 Pull in HPXML 4.2-rc1 schema
    
    git-subtree-dir: resources/hpxml-measures
    git-subtree-split: 0bc79d5d36a4dd1fc77be9c9ba30fbf330316884

--- HPXMLtoOpenStudio/resources/output.rb
+++ HPXMLtoOpenStudio/resources/output.rb
@@ -1311,10 +1311,20 @@ module Outputs
   # @param hpxml_bldgs [Array<HPXML::Building>] List of HPXML Building objects representing an individual dwelling unit
   # @return [Array] Rows of output data, with electric panel results appended
   def self.get_panel_results(hpxml_header, hpxml_bldgs)
+    results_out = []
+
+    # Currently, we only write this file if:
+    # (1) at least one load calculation type is specified
+    # (2) an electric panel is specified
+    if hpxml_header.service_feeders_load_calculation_types.empty?
+      return results_out
+    elsif hpxml_bldgs.map { |hpxml_bldg| hpxml_bldg.electric_panels.size }.sum == 0
+      return results_out
+    end
+
     line_break = nil
 
     # Summary breaker spaces
-    results_out = []
     results_out << ['Electric Panel Breaker Spaces: Heating Count', hpxml_bldgs.map { |hpxml_bldg| Outputs.get_total_breaker_spaces(hpxml_bldg)[0] }.sum(0)]
     results_out << ['Electric Panel Breaker Spaces: Cooling Count', hpxml_bldgs.map { |hpxml_bldg| Outputs.get_total_breaker_spaces(hpxml_bldg)[1] }.sum(0)]
     results_out << ['Electric Panel Breaker Spaces: Hot Water Count', hpxml_bldgs.map { |hpxml_bldg| Outputs.get_total_breaker_spaces(hpxml_bldg)[2] }.sum(0)]
