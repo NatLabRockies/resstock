0657cebe97b2a5417a8b1fe98d2166c65a9d34f1
Joe Robertson <joseph.robertson@nrel.gov>
2025-06-03 14:07:21 -0700

    Update config to automatically commenting a comparison plot.

---
+++ test/html_to_jpg.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+from html2image import Html2Image
+hti = Html2Image()
+
+html_file = 'test/base_results/comparisons/baseline/annual/results_output_building_type_energy_use.html'
+save_as = 'results_output_building_type_energy_use.jpg'
+
+# screenshot an HTML file
+hti.screenshot(
+    html_file=html_file,
+    save_as=save_as,
+    # size=(1920, 1080)
+    size=(3000, 1500)
+)
+
+shutil.move(save_as, os.path.join(os.path.dirname(html_file), save_as))
