40c86e20d056901de7a0814588deab6c9f57806b
Rajendra Adhikari <rajendraadhikari.ee@gmail.com>
2025-07-23 19:23:23 -0500

    Automatic retry

--- postprocessing/resstockpostproc/standard_plots/output_manager.py
+++ postprocessing/resstockpostproc/standard_plots/output_manager.py
@@ -44,6 +44,31 @@ class OutputManager:
         return full_path
 
     def save_plot(self, fig: Figure, path_seg: Path, df: pl.DataFrame, file_name: str) -> None:
+        """Save a plot to the output directory with automatic retry."""
+        max_retries = 3
+        delay_seconds = 3
+        # Might collide with generate_plots flow parallelly writing this
+        # So retry a few times if it fails
+        for attempt in range(max_retries):
+            try:
+                output_dir = self.get_output_dir(path_seg)
+                # Write files to their respective directories
+                if "html" in self.output_types:
+                    self.write_html(fig, output_dir, file_name)
+                if "svg" in self.output_types:
+                    self.write_svg(fig, output_dir, file_name)
+                if "parquet" in self.output_types:
+                    self.write_parquet(df, output_dir, file_name)
+                if "json" in self.output_types:
+                    self.write_json(fig, output_dir, file_name)
+                if "csv" in self.output_types:
+                    self.write_csv(df, output_dir, file_name)
+                break
+            except Exception as e:
+                if attempt < max_retries - 1:
+                    time.sleep(delay_seconds)
+                else:
+                    raise e
         """Save a plot to the output directory."""
         output_dir = self.get_output_dir(path_seg)
         # Write files to their respective directories
