910aa6d3a8f69038f80c87c5b3fc0a51984df35a
Andrew Parker <asparke2@gmail.com>
2025-09-25 15:45:12 -0600

    Rename columns for publication
    Rename columns to match ComStock and timeseries file conventions, including:
    two dots to separate units, standardize unit abbreviations, categorize more outputs as out.param, change the naming of savings columns.

--- postprocessing/resstockpostproc/process_metadata.py
+++ postprocessing/resstockpostproc/process_metadata.py
@@ -35,7 +35,7 @@ def publish_baseline_annual_results(base_raw_df: pl.LazyFrame) -> pl.LazyFrame:
     base_df = base_raw_df.filter(pl.col("completed_status") == "Success")
     base_df = get_transformed_cols(base_df, col_maps)
     base_df = base_df.with_columns(pl.lit(True).alias("applicability"))
-    base_df = base_df.with_columns([pl.lit(0).alias("upgrade"), pl.lit("Baseline").alias("upgrade_name")])
+    base_df = base_df.with_columns([pl.lit(0).alias("upgrade"), pl.lit("Baseline").alias("in.upgrade_name")])
     base_df = add_income_and_burden(base_df)
     base_df = add_county_column(base_df)
     base_df = add_puma_column(base_df)
@@ -99,7 +99,7 @@ def publish_upgrade_annual_results(
     upgrade_df = add_upgrade_columns(upgrade_df)
     upgrade_df = upgrade_df.with_columns(pl.lit(True).alias("applicability"))
     # get upgrade_name
-    upgrade_name_df = upgrade_df.select(pl.col("upgrade_name").first())
+    upgrade_name_df = upgrade_df.select(pl.col("in.upgrade_name").first())
     missing_bldgs_df = base_pub_df.join(
         upgrade_df,
         on="bldg_id",
@@ -110,7 +110,7 @@ def publish_upgrade_annual_results(
             pl.lit(False).alias("applicability"),
             pl.lit(upgrade_num).alias("upgrade"),
         ]
-    ).drop("upgrade_name")
+    ).drop("in.upgrade_name")
     upgrade_cols = upgrade_df.collect_schema().names()
     missing_bldgs_df = missing_bldgs_df.join(upgrade_name_df, how="cross")
     upgrade_df = pl.concat([upgrade_df, missing_bldgs_df], how="diagonal_relaxed")
@@ -154,9 +154,9 @@ def add_income_and_burden(df: pl.LazyFrame) -> pl.LazyFrame:
     # Calculate burden only when income is not null and positive
     burden = (
         pl.when(adj_income.is_not_null())
-        .then(pl.col("out.bills.all_fuels.usd") / adj_income * 100)
+        .then(pl.col("out.utility_bills.total_bill..usd") / adj_income * 100)
         .otherwise(None)
-        .alias("out.energy_burden.percentage")
+        .alias("out.energy_burden..percentage")
     )
 
     return df.with_columns([adj_income, burden])
@@ -170,14 +170,13 @@ def add_saving_cols(df: pl.LazyFrame, baseline_df: pl.LazyFrame) -> pl.LazyFrame
         "out.hot_water" in col or
         "out.panel" in col or
         "out.capacity" in col or
-        "out.unmet_hours.ev_driving" in col or
-        "out.component_load" in col
+        "out.unmet_hours.ev" in col
         )]
-    # Selectively include the following for panels
+    # Selectively include the following for params
     out_panel_cols = [col for col in all_cols if
-        "out.panel.load.total_load." in col
-        or "out.panel.load.occupied_capacity." in col
-        or "out.panel.breaker_space.occupied." in col
+        "out.params.panel_load_total_load" in col
+        or "out.params.panel_load_occupied_capacity" in col
+        or "out.params.panel_breaker_space_occupied" in col
     ]
     out_cols += out_panel_cols
 
@@ -186,33 +185,62 @@ def add_saving_cols(df: pl.LazyFrame, baseline_df: pl.LazyFrame) -> pl.LazyFrame
     )
     df_with_baseline = df.join(baseline_df_with_renamed, on="bldg_id", how="left")
     for col in out_cols:
-        if col.startswith("out.emissions"):
-            saving_col = (pl.col(f"baseline_{col}") - pl.col(col)).alias(
-                col.replace("out.emissions", "out.emissions_reduction")
-            )
-        else:
-            saving_col = (pl.col(f"baseline_{col}") - pl.col(col)).alias(f"{col}.savings")
+        new_col = col_name_to_savings(col)
+        saving_col = (pl.col(f"baseline_{col}") - pl.col(col)).alias(new_col)
         savings_cols.append(saving_col)
     return df_with_baseline.with_columns(savings_cols).drop([f"baseline_{col}" for col in out_cols])
 
 
+def col_name_to_savings(col_name):
+    converted_col_name = str(col_name)
+    svg_renames = {
+        '.energy_consumption': '.energy_savings',
+        '_bill..': '_bill_savings..',
+        '_daily_peak_': '_daily_peak_savings_',
+        '.peak_': '.peak_savings_',
+        '.energy_burden': '.energy_burden_savings',
+        '.unmet_hours': '.unmet_hours_reduction',
+        '.load.cooling.peak': '.load.cooling.peak_savings',
+        '.load.heating.peak': '.load.heating.peak_savings',
+        '.energy_delivered': '.energy_delivered_savings',
+        '.energy_solar_thermal': '.energy_solar_thermal_savings',
+        '.energy_tank_losses': '.energy_tank_losses_savings',
+        '.emissions.': '.emissions_reduction.',
+        'panel_load_total_load': 'panel_load_total_load_savings',
+        'panel_load_occupied_capacity': 'panel_load_occupied_capacity_savings',
+        'panel_breaker_space_occupied': 'panel_breaker_space_occupied_savings'
+    }
+    for bef, aft in svg_renames.items():
+        converted_col_name = converted_col_name.replace(bef, aft)
+
+    if converted_col_name == col_name:
+        raise ValueError(f"Cannot convert column name {col_name} to savings column")
+
+    return converted_col_name
+
+
+def remove_unused_consumption_cols(df: pl.LazyFrame) -> pl.LazyFrame:
+    # TODO
+    return df
+
+
 def add_panel_contraint_cols(df: pl.LazyFrame) -> pl.LazyFrame:
     all_cols = df.collect_schema().names()
-    amp_prefix = "out.panel.load.headroom_capacity."
+    amp_prefix = "out.params.panel_load_headroom_capacity."
     amp_cols = [col for col in all_cols if amp_prefix in col]
-    space_col = "out.panel.breaker_space.headroom.count"
+    space_col = "out.params.panel_breaker_space_headroom_count"
 
-    out_space_col = "out.panel.constraint.breaker_space"
+    out_space_col = "out.params.panel_constraint_breaker_space"
     space_constraint = pl.when(pl.col(space_col) < 0).then(True).otherwise(False).alias(out_space_col)
     ind_constraints = [space_constraint]
     overall_constraint = None
     for amp_col in amp_cols:
-        nec_method = amp_col.removeprefix(amp_prefix).removesuffix(".a")
-        out_amp_col = "out.panel.constraint.capacity." + nec_method
+        nec_method = amp_col.removeprefix(amp_prefix).removesuffix("..a")
+        out_amp_col = "out.params.panel_constraint_capacity." + nec_method
         amp_constraint = pl.when(pl.col(amp_col) < 0).then(True).otherwise(False).alias(out_amp_col)
         ind_constraints.append(amp_constraint)
 
-        out_overall_col = "out.panel.constraint.overall." + nec_method
+        out_overall_col = "out.params.panel_constraint_overall." + nec_method
         overall_constraint = pl.coalesce(
             pl.when(pl.col(out_amp_col) & pl.col(out_space_col)).then(pl.lit("Capacity and Space Constrained")),
             pl.when(pl.col(out_amp_col) & ~pl.col(out_space_col)).then(pl.lit("Capacity Constrained Only")),
@@ -267,7 +295,7 @@ def add_upgrade_columns(lf: pl.LazyFrame) -> pl.LazyFrame:
         upgrade_lf.unpivot(
             index="bldg_id",
             on=upgrade_cols,
-            variable_name="upgrade_name",
+            variable_name="in.upgrade_name",
             value_name="upgrade_value",
         )
         .drop_nulls("upgrade_value")
@@ -294,12 +322,16 @@ def reorder_columns(lf: pl.LazyFrame, col_maps: Sequence[dict], is_baseline: boo
     all_defined_cols = [col_map["published_name"] for col_map in col_maps]
     extra_cols = all_df_cols - set(all_defined_cols)
     if extra_cols:
-        print(f"Extra columns in output data not defined in publication column definition: {extra_cols}")
+        print(f"Extra columns in output data not defined in publication column definition:")
+        for c in sorted(extra_cols):
+            print(f"Extra column: {c}")
     missing_cols = [col for col in set(all_defined_cols) - all_df_cols if not col.startswith("upgrade.")]
     if is_baseline:
         missing_cols = [col for col in missing_cols if not ("savings" in col or "reduction" in col)]
     if missing_cols:
-        print(f"Missing columns in output data that is defined in publication column definition: {missing_cols}")
+        print(f"Missing columns in output data that are defined in publication column definition:")
+        for c in sorted(missing_cols):
+            print(f"Missing column: {c}")
     available_cols = [col for col in all_defined_cols if col in all_df_cols]
     return lf.select(available_cols)
 
