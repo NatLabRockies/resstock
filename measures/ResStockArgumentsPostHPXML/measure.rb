2432a1f2c7bcc9506fd3e4cf2b2273a0a85fd176
Joe Robertson <joseph.robertson@nrel.gov>
2025-11-19 15:33:22 -0700

    Update config and ymls with new image and sha.

--- measures/ResStockArgumentsPostHPXML/measure.rb
+++ measures/ResStockArgumentsPostHPXML/measure.rb
@@ -37,30 +37,6 @@ class ResStockArgumentsPostHPXML < OpenStudio::Measure::ModelMeasure
     full_measure_path = File.join(File.dirname(__FILE__), '..', 'ResStockArguments', 'measure.rb')
     measure_arguments = get_measure_instance(full_measure_path).arguments(model)
     measure_arguments.each do |arg|
-      # # Convert to optional argument for the unit test
-      # # Replace all of this if https://github.com/NREL/OpenStudio/issues/5469 is addressed
-      # case arg.type.valueName.downcase
-      # when 'choice'
-      # new_arg = OpenStudio::Measure::OSArgument.makeChoiceArgument(arg.name, arg.choiceValues, false)
-      # new_arg.setDefaultValue(arg.defaultValueAsString) if arg.hasDefaultValue
-      # when 'boolean'
-      # new_arg = OpenStudio::Measure::OSArgument.makeBoolArgument(arg.name, false)
-      # new_arg.setDefaultValue(arg.defaultValueAsBool) if arg.hasDefaultValue
-      # when 'string'
-      # new_arg = OpenStudio::Measure::OSArgument.makeStringArgument(arg.name, false)
-      # new_arg.setDefaultValue(arg.defaultValueAsString) if arg.hasDefaultValue
-      # when 'double'
-      # new_arg = OpenStudio::Measure::OSArgument.makeDoubleArgument(arg.name, false)
-      # new_arg.setDefaultValue(arg.defaultValueAsDouble) if arg.hasDefaultValue
-      # when 'integer'
-      # new_arg = OpenStudio::Measure::OSArgument.makeIntegerArgument(arg.name, false)
-      # new_arg.setDefaultValue(arg.defaultValueAsInteger) if arg.hasDefaultValue
-      # else
-      # fail "Unhandled argument type: #{arg.type.valueName.downcase}"
-      # end
-      # new_arg.setDisplayName(arg.displayName.to_s)
-      # new_arg.setDescription(arg.description.to_s)
-      # new_arg.setUnits(arg.units.to_s)
       arg.setRequired(false)
       args << arg
     end
