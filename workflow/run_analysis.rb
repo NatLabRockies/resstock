a8e07e4d41ea1f557f50a215701701592f6255c8
Joe Robertson <joseph.robertson@nrel.gov>
2026-06-16 14:31:48 -0700

    Try regular old python vs python3.

--- workflow/run_analysis.rb
+++ workflow/run_analysis.rb
@@ -498,9 +498,9 @@ def create_buildstock_csv(project_dir, sampler_type, num_samples, outfile)
     r = RunSampling.new
     r.run(project_dir, num_samples, outfile)
   elsif sampler_type == 'residential_stratified'
-    # command = "python #{File.dirname(__FILE__)}/../samplers/stratified/sampler/run_sampler.py sample"
+    command = "python #{File.dirname(__FILE__)}/../samplers/stratified/sampler/run_sampler.py sample"
     # command = "uv run python #{File.dirname(__FILE__)}/../samplers/stratified/sampler/run_sampler.py sample"
-    command = "resstock-sampler sample"
+    # command = "resstock-sampler sample"
     command += " -p \"#{project_dir}\""
     command += " -n \"#{num_samples}\""
     command += " -o \"#{outfile}\""
